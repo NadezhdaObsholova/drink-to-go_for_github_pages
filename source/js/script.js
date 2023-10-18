ea24c229e79c5482f3c933fc421f08271855175b
NadezhdaObsholova <obsholova.n@gmail.com>
2023-10-19 02:10:00 +0300

    создание nouislider

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,23 @@
-/* в этот файл добавляет скрипты*/
+const sliderElement = document.querySelector('.price-slider__holder');
+
+const minValueElement = document.querySelector('.price-slider__value-min');
+const maxValueElement = document.querySelector('.price-slider__value-max');
+
+minValueElement.value = 0;
+maxValueElement.value = 900;
+
+noUiSlider.create(sliderElement, {
+
+  start: [0, 900],
+  connect: true,
+  step: 1,
+  range: {
+    'min': [0],
+    'max': [1000]
+  }
+});
+
+sliderElement.noUiSlider.on('update', () => {
+  minValueElement.value = Math.round(sliderElement.noUiSlider.get().shift(-1));
+  maxValueElement.value = Math.round(sliderElement.noUiSlider.get().slice(1));
+});
