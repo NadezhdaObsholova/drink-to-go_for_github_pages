7b242d80999eb2a38e9fdad575d9d6da2291b539
NadezhdaObsholova <obsholova.n@gmail.com>
2023-10-20 23:04:10 +0300

    все

--- source/js/script.js
+++ source/js/script.js
@@ -39,9 +39,97 @@ menuButtonElement.addEventListener('click', onClickMenuButtonElement);
 
 //slider
 
-const buttonPrevElement = document.querySelector('.slider__button-prev');
-const buttonNextElement = document.querySelector('.slider__button-next');
-const slidesElements = Array.from(document.querySelectorAll('.slider__item'));
-const sliderPagination = Array.from(document.querySelectorAll('.slider__padination-item'));
-const slideCount = slidesElements.length;
-let currentSlide = 0;
+
+// слайдер
+
+const slides = Array.from(document.querySelectorAll('.slider__item'));
+const prevButton = document.querySelector('.slider__button-prev');
+const nextButton = document.querySelector('.slider__button-next');
+const paginationMark = Array.from(document.querySelectorAll('.slider__padination-item'));//путь внутри кнопки
+
+let currentSlideIndex = 0;
+
+// Функция, которая скрывает все слайды и показывает только текущий слайд.
+
+const showSlide = (index) => {
+  slides.forEach((slide, i) => {
+    if (i === index) {
+      slide.classList.remove('visually-hidden');
+    } else {
+      slide.classList.add('visually-hidden');
+    }
+  });
+};
+
+//пагинация
+
+const paginationMarkCurrent = () => {
+  paginationMark.forEach((item, i) => {
+    if (i === currentSlideIndex) {
+      item.classList.add('slider__padination-item--current');
+    } else {
+      item.classList.remove('slider__padination-item--current');
+    }
+  });
+};
+
+
+// Показать первый слайд при загрузке страницы.
+showSlide(currentSlideIndex);
+// Показать первую пагинацию при загрузке страницы.
+paginationMarkCurrent();
+//
+if (currentSlideIndex === 0) {
+  prevButton.setAttribute('disabled', true);
+}
+
+// Обработчик для кнопки "Следующий слайд".
+nextButton.addEventListener('click', () => {
+  if (currentSlideIndex < slides.length - 1) {
+    currentSlideIndex = currentSlideIndex + 1;
+    showSlide(currentSlideIndex);
+    paginationMarkCurrent();
+  }
+  if (currentSlideIndex === slides.length - 1) {
+    nextButton.setAttribute('disabled', true);
+  }
+
+  prevButton.removeAttribute('disabled');
+
+});
+
+// Обработчик для кнопки "Предыдущий слайд".
+prevButton.addEventListener('click', () => {
+  if (currentSlideIndex > 0) {
+    currentSlideIndex = currentSlideIndex - 1;
+    showSlide(currentSlideIndex);
+    paginationMarkCurrent();
+  }
+  if (currentSlideIndex === 0) {
+    prevButton.setAttribute('disabled', true);
+  }
+
+  nextButton.removeAttribute('disabled');
+});
+
+// Обработчик для пагинации.
+sliderPagination.forEach((paginationButton, i) => {
+  paginationButton.addEventListener('click', () => {
+    currentSlideIndex = i;
+    showSlide(currentSlideIndex);
+    paginationMarkCurrent();
+
+    if (currentSlideIndex === 0) {
+      prevButton.setAttribute('disabled', true);
+    } else {
+      prevButton.removeAttribute('disabled');
+    }
+
+    if (currentSlideIndex === slides.length - 1) {
+      nextButton.setAttribute('disabled', true);
+    } else {
+      nextButton.removeAttribute('disabled');
+    }
+  });
+});
+
